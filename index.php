962af33b70f34d351e1cb601ed3d526aaa16f70e
Anwar <justdoit2045@gmail.com>
2017-02-07 18:01:28 +0545

    changes

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="Boomer" />
+
+	<title>Day 1</title>
+</head>
+
+<body>
+<strong>This is bold</strong>
+<em>This is italics</em>
+
+<h1>This is heading 1</h1>
+x<sup>2</sup>
+<br />
+H<sub>2</sub>O
+
+<p>
+adfadfadsadf adf
+ad fadf adf
+adf adadf ad
+adf adfad fa
+</p>
+
+<p>
+adfadfadsadf adf
+ad fadf adf
+adf adadf ad
+adf adfad fa
+</p>
+
+<p>
+adfadfadsadf adf
+ad fadf adf
+adf adadf ad
+adf adfad fa
+</p>
+
+<span>adfadfadf</span><span>adfadfadf</span>
+<div>adfadfadf</div>
+</body>
+</html>
