12b502aa46c4bd90020bebc474f4ba2a7383925c
firoj alam <firoj alam>
2017-02-08 16:24:37 +0545

    change

---
+++ firoj.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML> 
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="Boomer" />
+
+	<title>Day 1</title>
+</head>
+
+<body>
+<strong>This is bold</strong>
+<em>This is italics</em>
+
+<h1>This is heading 1</h1>
+x<sup>2</sup>
+<br />
+H<sub>2</sub>O
+
+<p>
+adfadfadsadf adf
+ad fadf adf
+adf adadf ad
+adf adfad fa
+</p>
+
+<p>
+adfadfadsadf adf
+ad fadf adf
+adf adadf ad
+adf adfad fa
+</p>
+
+<p>
+adfadfadsadf adf
+ad fadf adf
+adf adadf ad
+adf adfad fa
+</p>
+
+<span>adfadfadf</span><span>adfadfadf</span>
+<div>adfadfadf</div>
+</body>
+</html><!DOCTYPE HTML> 
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="Boomer" />
+
+	<title>Day 1</title>
+</head>
+
+<body>
+<strong>This is bold</strong>
+<em>This is italics</em>
+
+<h1>This is heading 1</h1>
+x<sup>2</sup>
+<br />
+H<sub>2</sub>O
+
+<h1>firoj alam</h1>
+<h2>i m a boy</h2>
+
+
+
+
+<span>adfadfadf</span><span>adfadfadf</span>
+<div>adfadfadf</div>
+</body>
+</html>
