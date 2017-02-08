fd14d3ad29df22c6133ea813e4a5321971d908b0
Javedc Ali <alijaved424122@gmail.com>
2017-02-08 16:10:04 +0545

    change

---
+++ javed.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="Boomer" />
+
+	<title>Day 1</title>
+</head>
+
+<body> 
+<strong>This is Bold</strong>
+<br />
+<em>This is Italic</em>
+<br />
+H<sub>2</sub>o
+<br />
+x<sup>2</sup>
+<br />
+<span>welfkhweflk</span>
+<span>kfjhgawlkfhwe</span>
+<div>kjhwelfkwej</div>
+
+
+
+</body>
+</html>
